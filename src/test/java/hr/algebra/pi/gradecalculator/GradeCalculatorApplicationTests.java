ed8516b6d2ce2cfcbf95ca664ec12477e6a8d2a0
ViktorPetrina_ <viktor.petrina01@gmail.com>
2024-12-05 14:12:41 +0100

    baseline project

---
+++ src/test/java/hr/algebra/pi/gradecalculator/GradeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.pi.gradecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
